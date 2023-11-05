84bfc49849b9e36767aebadf9af95eac76af28d5
root <root@KVL-TUF>
2023-11-05 21:05:02 +0000

    task2 completed?

--- task2/f1_fsm.sv
+++ task2/f1_fsm.sv
@@ -2,19 +2,43 @@ module f1_fsm (
     input logic             clk, // clock
     input logic             rst, // reset
     input logic             en,  // enable
-    output logic [7:1]      data_out // pseudo-random output
+    output logic [7:0]      data_out // output
 );
 
-    logic[7:1] sreg;        // remember to initialise the "shift register" here
+    typedef enum  {S0,S1,S2,S3,S4,S5,S6,S7,S8} states;
+    states curr_state, nxt_state;
 
-always_ff @ (posedge clk, posedge rst)
-    if (rst) 
-        sreg <= 7'b1;
-    else if (en)
-        sreg <= {sreg[6:1], sreg[7] ^ sreg[3]};
+always_comb // Next state logic
+    case (curr_state)
+        S0: nxt_state = S1;
+        S1: nxt_state = S2;
+        S2: nxt_state = S3;
+        S3: nxt_state = S4;
+        S4: nxt_state = S5;
+        S5: nxt_state = S6;
+        S6: nxt_state = S7;
+        S7: nxt_state = S8;
+        S8: nxt_state = S0;
+        default: nxt_state = S0;
+    endcase  
 
-assign data_out = sreg; 
+always_comb // Output logic
+    case (curr_state)
+        S0: data_out = 8'b0;
+        S1: data_out = 8'b1;
+        S2: data_out = 8'b11;
+        S3: data_out = 8'b111;
+        S4: data_out = 8'b1111;
+        S5: data_out = 8'b11111;
+        S6: data_out = 8'b111111;
+        S7: data_out = 8'b1111111;
+        S8: data_out = 8'b11111111;
+        default: data_out = 8'b0;
+    endcase
 
-// Primitive polynomial: 1 + X^3 + X^7
+always_ff @(posedge clk)// State transition
+    if (rst)    curr_state <= S1;
+    else if (en)       curr_state <= nxt_state;
+    
 
 endmodule
--- task2/f1_fsm_tb.cpp
+++ task2/f1_fsm_tb.cpp
@@ -1,22 +1,20 @@
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-#include "Vlfsr.h"
+#include "Vf1_fsm.h"
 
 #include "vbuddy.cpp"
 #define MAX_SIM_CYC 1000000
 
 int main(int argc, char **argv, char **env) {
-  int simcyc;     // simulation clock count
-  int tick;       // each clk cycle has two ticks for two edges
+  int simcyc;
+  int tick; 
 
   Verilated::commandArgs(argc, argv);
-  // init top verilog instance
-  Vlfsr* top = new Vlfsr;
-  // init trace dump
+  Vf1_fsm* top = new Vf1_fsm;
   Verilated::traceEverOn(true);
   VerilatedVcdC* tfp = new VerilatedVcdC;
   top->trace (tfp, 99);
-  tfp->open ("lfsr.vcd");
+  tfp->open ("f1_fsm.vcd");
  
   // init Vbuddy
   if (vbdOpen()!=1) return(-1);
@@ -26,7 +24,7 @@ int main(int argc, char **argv, char **env) {
   // initialize simulation input 
   top->clk = 1;
   top->rst = 1;
-  top->en = 1;
+  top->en = 0; // init not enabled, so rotary doesn't do anything
   top->data_out = 0;
 
   for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
